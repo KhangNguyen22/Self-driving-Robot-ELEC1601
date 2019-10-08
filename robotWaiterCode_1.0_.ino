0559bbf1597ae077fd2ce2c92f0dce2a41a5e062
Minh Khang Johannes Leon Nguyen <mngu3698@uni.sydney.edu.au>
2019-10-08 17:31:21 +1100

    Initial commit scaffold for code

---
+++ robotWaiterCode_1.0_.ino
@@ -0,0 +1,37 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// Move Arduino forward
+void forward(){
+  
+  }
+
+// Move Arduino backward
+void backward(){
+  
+  }
+
+ // Move Arduino left
+void left(){
+  
+  }
+  
+// Move Arduino right
+void right(){
+  }
+
+// Error, lights up red LED
+
+void error(){
+  }
+
+// Detect if there is a line
+void detectIntersection(){
+  }
